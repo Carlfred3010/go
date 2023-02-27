ee5e01bb68a6d709d8d7a14520f6f9c0f10ca5bc
Carl Theodate <carl.theodate@ufl.edu>
2023-02-27 01:26:16 -0500

    fixed database name

---
+++ goApp/database/database.go
@@ -0,0 +1,36 @@
+package databse
+
+import (
+	"log"
+	"os"
+
+	"github.com/Carlfred3010/goApp/models"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type DBinstance struct {
+	Db *gorm.DB
+}
+
+var Database DBinstance
+
+func ConnectDb() {
+
+	db, err := gorm.Open(sqlite.Open("api.db"), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database\n", err.Error())
+		os.Exit(2)
+	}
+
+	log.Println("Connected to database successfully")
+
+	db.Logger = logger.Default.LogMode(logger.Info)
+	log.Println("running Migrations")
+	db.AutoMigrate(&models.User{}, &models.Stock{})
+
+	Database = DBinstance{Db: db}
+
+}
